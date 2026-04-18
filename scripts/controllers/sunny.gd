30e4e140abfad448a9b3261465d9974ffeddca25
PaulinaArr <ichigokosama@gmail.com>
2026-04-18 02:08:50 -0400

    eep break

--- scripts/controllers/sunny.gd
+++ scripts/controllers/sunny.gd
@@ -12,5 +12,12 @@ func _process(delta: float) -> void:
 #keep Sunny inside y bounds 
 	position.y = clamp(position.y, 0, 720)
 
+#makes colition with obstacles
 func _on_area_2d_body_entered(body):
+	#when touch, reload scene
 	get_tree().reload_current_scene()
+
+#Okay Ichi don't forget, you are making the cutscenes to implement them straight away
+#We have basic movement, up and down, it moves to the side.
+#Sunny can colide with obstacles now, we need to draw more obstacles 
+# Feather system, half build, finish that first.
